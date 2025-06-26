b629cb0c387ec7b6dc2171fd808d11d8b9c0a5a1
alex <alex@fantium.com>
2025-06-26 16:00:51 +0200

    Refactor to use .env

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBZrbNeLOW838LHqSzvCftgyt9OtKbeuKo",
-    authDomain: "cryptotracker-edcb2.firebaseapp.com",
-    projectId: "cryptotracker-edcb2",
-    storageBucket: "cryptotracker-edcb2.firebasestorage.app",
-    messagingSenderId: "154402800443",
-    appId: "1:154402800443:web:d51d6162e3469a578c6d1e",
-    measurementId: "G-4CG61CWG69"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
